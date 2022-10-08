603e3a42ef748309f2337337cd96ac171f0b6da6
himsapphire <87421281+himsapphire@users.noreply.github.com>
2022-10-08 12:50:50 +0530

    Update Starters_59.cpp

--- Starters_59.cpp
+++ Starters_59.cpp
@@ -11,6 +11,7 @@ int main() {
 	    cin>>n;
 	    int arr[n];
 	   long long int temp1=0,sum=0,temp2=0;
+		//first of all find the maximum subarray sum from array A, HERE, sum is storing that
 	    for(int i=0;i<n;i++){
 	        cin>>arr[i];
 	        temp1+=arr[i];
@@ -24,6 +25,7 @@ int main() {
 	    
 	    cin>>m;
 	    int arr1[m];
+		//from array B add all the positive values and stored in temp2
 	    for(int i=0;i<m;i++){
 	        cin>>arr1[i];
 	        if(arr1[i]>0)
